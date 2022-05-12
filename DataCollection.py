33a8e9a703efb68aa0cb53251d7b726a9ae3b12d
Booooms <1222kbj@naver.com>
2022-05-12 22:24:49 +0900

    [update] openCV

--- DataCollection.py
+++ DataCollection.py
@@ -1,7 +1,8 @@
 # 실제 트랙을 주행하며 데이터 수집
+# openCV 필터 적용
+import time
 import cv2
 import RPi.GPIO as GPIO
-import time
 
 PWMA = 18
 AIN1 = 22
@@ -70,59 +71,63 @@ R_Motor.start(0)
 
 speedSet = 50
 
+camera = cv2.VideoCapture(-1)
+camera.set(3, 640)
+camera.set(4, 480)
+  
 def main():
-    camera = cv2.VideoCapture(-1)
-    camera.set(3, 640)
-    camera.set(4, 480)
     filepath = "/home/pi/AI_CAR/video/train"
     i = 0
     carState = "stop"
-    while( camera.isOpened() ):
+    try:
+        while True:
+            
+            keyValue = cv2.waitKey(1)
         
-        keyValue = cv2.waitKey(10)
-        
-        if keyValue == ord('q'):
-            break
-        elif keyValue == 82:
-            print("go")
-            carState = "go"
-            motor_go(speedSet)
-        elif keyValue == 84:
-            print("stop")
-            carState = "stop"
-            motor_stop()
-        elif keyValue == 81:
-            print("left")
-            carState = "left"
-            motor_left(speedSet)
-        elif keyValue == 83:
-            print("right")
-            carState = "right"
-            motor_right(speedSet)
-        
-        _, image = camera.read()
-        image = cv2.flip(image,-1)
-        cv2.imshow('Original', image)
-        
-        height, _, _ = image.shape
-        save_image = image[int(height/2):,:,:]
-        save_image = cv2.cvtColor(save_image, cv2.COLOR_BGR2YUV)
-        save_image = cv2.GaussianBlur(save_image, (3,3), 0)
-        save_image = cv2.resize(save_image, (200,66))
-        cv2.imshow('Save', save_image)
-        
-        if carState == "left":
-            cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 45), save_image)
-            i += 1
-        elif carState == "right":
-            cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 135), save_image)
-            i += 1
-        elif carState == "go":
-            cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 90), save_image)
-            i += 1
-        
-    cv2.destroyAllWindows()
-    
+            if keyValue == ord('q'):
+                break
+            elif keyValue == 82:
+                print("go")
+                carState = "go"
+                motor_go(speedSet)
+            elif keyValue == 84:
+                print("stop")
+                carState = "stop"
+                motor_stop()
+            elif keyValue == 81:
+                print("left")
+                carState = "left"
+                motor_left(speedSet)
+            elif keyValue == 83:
+                print("right")
+                carState = "right"
+                motor_right(speedSet)
+                
+            _, image = camera.read()
+            image = cv2.flip(image,-1)
+            height, _, _ = image.shape
+            image = image[int(height/2):,:,:]
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+            image = cv2.resize(image, (200,66))
+            image = cv2.GaussianBlur(image,(5,5),0)
+            _,image = cv2.threshold(image,160,255,cv2.THRESH_BINARY_INV)
+            
+            if carState == "left":
+                cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 45), image)
+                i += 1
+            elif carState == "right":
+                cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 135), image)
+                i += 1
+            elif carState == "go":
+                cv2.imwrite("%s_%05d_%03d.png" % (filepath, i, 90), image)
+                i += 1
+            
+            cv2.imshow('Original', image)
+            
+    except KeyboardInterrupt:
+        pass
+
 if __name__ == '__main__':
     main()
+    cv2.destroyAllWindows()
     GPIO.cleanup()
